7e5ce78f5bd9dd135325757f06101b4cebf3afcc
coen17st <c.stam.mail@gmail.com>
2021-05-27 22:03:28 +0200

    test env

--- .drone/deploy.sh
+++ .drone/deploy.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 set -e
 
+# PULL IMAGE FROM PRIVATE REGISTRY
 docker pull docker-registry.theautomation.nl/coen/prd-node-red
 
+# RUN DOCKER CONTAINER
 docker run -d \
 -p 1880:1880 \
 -v /home/coen/docker-home-services/prd-node-red/data:/data \
 --network=home-services \
---name prd-node-red \
+--name prd-node-red-app \
 --restart=always \
+--env PGID=1000 \
+--env PUID=1000 \
+--env TZ=Europe/Amsterdam \
 docker-registry.theautomation.nl/coen/prd-node-red:latest
--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM nodered/node-red:1.3.1
 
 WORKDIR /usr/src/node-red
 
-# ADD EXTRA NODES IN THE package.json FILE.
+# ADD EXTRA NODES IN THE /data/package.json FILE.
 COPY data/package.json .
 
 RUN npm install --unsafe-perm --no-update-notifier --no-fund --only=production
