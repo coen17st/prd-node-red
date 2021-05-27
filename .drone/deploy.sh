f9cc7afe1d9e7a2c13ffa8ec0b39dd9874283cac
coen17st <c.stam.mail@gmail.com>
2021-05-27 21:32:49 +0200

    another deploy test

--- .drone/deploy.sh
+++ .drone/deploy.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 set -e
 
-docker pull docker-registry.theautomation.nl/coen/prd-node-red \
-&& docker run --rm -d \
+docker pull docker-registry.theautomation.nl/coen/prd-node-red
+
+docker run -d \
 -p 1880:1880 \
--v $(pwd)/data:/data \
+-v /home/coen/docker-home-services/prd-node-red/data:/data \
 --network=home-services \
 --name prd-node-red \
+--restart=always \
 docker-registry.theautomation.nl/coen/prd-node-red:latest
