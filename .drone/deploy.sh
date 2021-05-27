9357732f984f76fa7b9162f891b5b8a56ef47246
coen17st <c.stam.mail@gmail.com>
2021-05-27 22:08:17 +0200

    test env in sh

--- .drone/deploy.sh
+++ .drone/deploy.sh
@@ -2,16 +2,16 @@
 set -e
 
 # PULL IMAGE FROM PRIVATE REGISTRY
-docker pull docker-registry.theautomation.nl/coen/prd-node-red
+docker pull docker-registry.theautomation.nl/coen/${DRONE_REPO_NAME}
 
 # RUN DOCKER CONTAINER
 docker run -d \
 -p 1880:1880 \
--v /home/coen/docker-home-services/prd-node-red/data:/data \
+-v /home/coen/docker-home-services/${DRONE_REPO_NAME}/data:/data \
 --network=home-services \
---name prd-node-red-app \
+--name ${DRONE_REPO_NAME} \
 --restart=always \
 --env PGID=1000 \
 --env PUID=1000 \
 --env TZ=Europe/Amsterdam \
-docker-registry.theautomation.nl/coen/prd-node-red:latest
+docker-registry.theautomation.nl/coen/${DRONE_REPO_NAME}:latest
