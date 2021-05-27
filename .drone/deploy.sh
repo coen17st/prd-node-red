c1684c4d235837907dd65f9ce8d7ba395aab6b1e
coen17st <c.stam.mail@gmail.com>
2021-05-27 22:27:24 +0200

    testing remove container

--- .drone/deploy.sh
+++ .drone/deploy.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
 set -e
 
-# PULL IMAGE FROM PRIVATE REGISTRY
-docker pull docker-registry.theautomation.nl/coen/${DRONE_REPO_NAME}
+# VARIABLES
+container_name=${DRONE_REPO_NAME}
+
+# PULL DOCKER IMAGE FROM PRIVATE DOCKER REGISTRY
+docker pull docker-registry.theautomation.nl/coen/${container_name}
+
+# STOP DOCKER CONTAINER
+docker stop ${container_name}
+
+# REMOVE DOCKER CONTAINER
+docker rm ${container_name}
 
 # RUN DOCKER CONTAINER
 docker run -d \
 -p 1880:1880 \
--v /home/coen/docker-home-services/${DRONE_REPO_NAME}/data:/data \
+-v /home/coen/docker-home-services/${container_name}/data:/data \
 --network=home-services \
---name ${DRONE_REPO_NAME} \
+--name ${container_name} \
 --restart=always \
 --env PGID=1000 \
 --env PUID=1000 \
 --env TZ=Europe/Amsterdam \
-docker-registry.theautomation.nl/coen/${DRONE_REPO_NAME}:latest
+docker-registry.theautomation.nl/coen/${container_name}:latest
--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 [![Build Status](https://drone.theautomation.nl/api/badges/coen17st/prd-node-red/status.svg)](https://drone.theautomation.nl/coen17st/prd-node-red)
 
-Node-Red
-test 1 2 3
+[Node-RED](https://nodered.org/): A programming tool for wiring together hardware devices, APIs and online services in new and interesting ways.
